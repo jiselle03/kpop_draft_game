2fcf33171ca12b43f6ca7c795dc830977ba16077
Jiselle Liu <jiselle03@gmail.com>
2025-11-01 00:52:58 +0800

    Set up codex and spec kit

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false,
+  arrowParens: "always",
   plugins: ["prettier-plugin-tailwindcss"],
 };
